929351761bbae9d2a07ecf5f078012a1898c2ec9
Beatriz Rivera <rivers.tris@gmail.com>
2017-06-16 18:58:32 +0800

    initial commit

---
+++ ArraysDS.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class ArraysDS{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		
+		String str = in.nextLine();
+        
+		System.out.println(new StringBuilder(str).reverse().toString());
+    }
+}
